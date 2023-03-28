870b458232483e212ac675c5baa8ff5fd4c89228
Miracle2333 <kangzhaogeo@gmail.com>
2023-03-28 06:25:00 +0000

    add downstream tasks config

--- scripts/run_train_semi.sh
+++ scripts/run_train_semi.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=.
-nohup python -m paddle.distributed.launch --gpus 1,2,3,4 tools/train.py \
- -c configs/semi_det/semi_detr/dino_ssod_burnin.yml \
-  --eval --use_vdl=true --vdl_log_dir=vdl_dir/5_lr_10 1>logs/5_sup.log 2>logs/5_sup.err &
+nohup python -m paddle.distributed.launch --gpus 0,1,2,3,4,5,6,7 --log_dir log_dir/semi_sslad_10 tools/train.py \
+ -c configs/semi_det/semi_detr/dino_ssod_sslad.yml \
+  --eval --use_vdl=true --vdl_log_dir=vdl_dir/ssod_sslad >/dev/null 2>&1 &
