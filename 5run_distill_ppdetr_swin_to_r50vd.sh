855c6d28801a727902f0c0d628ac4ca6cd57f55e
Miracle2333 <kangzhaogeo@gmail.com>
2023-04-03 11:07:08 +0000

    add inheriting true

--- 5run_distill_ppdetr_swin_to_r50vd.sh
+++ 5run_distill_ppdetr_swin_to_r50vd.sh
@@ -4,12 +4,12 @@ job_name=distill_stu_ppdino_swin_r50vd_12e
 config=configs/dino/ppdino_distill/distill_stu_ppdino_r50vd_12e.yml
 tea_config=configs/dino/ppdino_distill/distill_tea_ppdino_swin_to_r50vd_12e.yml
 
-log_dir=log_dir/${job_name}
+log_dir=log_dir/swin_r50_kd_detr
 weights=output/distill_tea_ppdino_swin_to_r50vd_12e/0.pdparams
 
 # 1. training
 #CUDA_VISIBLE_DEVICES=2 python3.7 tools/train.py -c ${config} --slim_config ${tea_config} --eval #--amp
-python3.7 -m paddle.distributed.launch --log_dir=${log_dir} --gpus 0,1,2,3,4,5,6,7 tools/train.py -c ${config} --slim_config ${tea_config} --eval #--amp
+nohup python3.7 -m paddle.distributed.launch --log_dir=${log_dir} --gpus 0,1,2,3,4,5,6,7 tools/train.py -c ${config} --slim_config ${tea_config} --eval >/dev/null 2>&1 &
 
 # 2. eval
 #CUDA_VISIBLE_DEVICES=0 python3.7 tools/eval.py -c ${config} -o weights=https://paddledet.bj.bcebos.com/models/${job_name}.pdparams
