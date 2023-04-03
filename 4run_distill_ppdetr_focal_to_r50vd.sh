0da274da8ffa8e55821b08ab48e69ae06ee62a4b
Miracle2333 <kangzhaogeo@gmail.com>
2023-04-03 12:04:21 +0000

    add focal2r50 config

--- 4run_distill_ppdetr_focal_to_r50vd.sh
+++ 4run_distill_ppdetr_focal_to_r50vd.sh
@@ -1,15 +1,15 @@
 export FLAGS_allocator_strategy=auto_growth
-job_name=distill_stu_ppdino_r50vd_12e
+job_name=distill_stu_ppdino_focal_r50vd_12e
 
 config=configs/dino/ppdino_distill/distill_stu_ppdino_r50vd_12e.yml
 tea_config=configs/dino/ppdino_distill/distill_tea_ppdino_focal_to_r50vd_12e.yml
 
-log_dir=log_dir/${job_name}
+log_dir=log_dir/focal_r50_kd_detr
 weights=output/distill_tea_ppdino_swin_to_r50vd_12e/0.pdparams
 
 # 1. training
 #CUDA_VISIBLE_DEVICES=2 python3.7 tools/train.py -c ${config} --slim_config ${tea_config} --eval #--amp
-python3.7 -m paddle.distributed.launch --log_dir=${log_dir} --gpus 0,1,2,3,4,5,6,7 tools/train.py -c ${config} --slim_config ${tea_config} --eval #--amp
+python python3.7 -m paddle.distributed.launch --log_dir=${log_dir} --gpus 0,1,2,3,4,5,6,7 tools/train.py -c ${config} --slim_config ${tea_config} --eval >/dev/null 2>&1 &
 
 # 2. eval
 #CUDA_VISIBLE_DEVICES=0 python3.7 tools/eval.py -c ${config} -o weights=https://paddledet.bj.bcebos.com/models/${job_name}.pdparams
